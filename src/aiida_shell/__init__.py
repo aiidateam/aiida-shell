9379bd124c3a565061169bc32113b00ca4e28f4e
Julian Geiger <julian.geiger@gmx.net>
2026-01-14 11:19:20 +0100

    Release `v0.8.2`

--- src/aiida_shell/__init__.py
+++ src/aiida_shell/__init__.py
@@ -1,5 +1,5 @@
 """AiiDA plugin that makes running shell commands easy."""
-__version__ = '0.8.1'
+__version__ = '0.8.2'
 
 from .calculations import ShellJob
 from .data import EntryPointData, PickledData, ShellCode
